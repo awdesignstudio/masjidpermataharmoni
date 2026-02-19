8869a5819e6510ec2fac3f2df4c618a78a7a9932
awdesignstudio <end.joy.urself@gmail.com>
2026-02-19 14:29:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,41 @@
-const CACHE_NAME = 'masjid-portal-v3';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'masjid-portal-v2.1'; // GANTI versi setiap kali update kode
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(assets);
-    })
-  );
-});
+// File yang mau di-cache (tambah/kurangi sesuai kebutuhan)
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-192x192.png'
+];
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
-});
 self.addEventListener('install', event => {
+  // Paksa Service Worker baru langsung aktif tanpa menunggu tab ditutup
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Hapus cache versi lama secara otomatis
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
